06a5a182dd6aaee619868747779a09a5e744bde2
Ayush kumar <151223800+CodeBy-Ayush@users.noreply.github.com>
2026-01-04 15:05:34 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,8 @@
+//Main Body Of this App
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App.jsx' // Import App.jsx 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
