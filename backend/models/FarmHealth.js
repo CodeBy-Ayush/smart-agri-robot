17ac772edfbf8fc428351f687a88570540262c22
Ayush kumar <151223800+CodeBy-Ayush@users.noreply.github.com>
2026-01-09 20:44:51 +0530

    Update FarmHealth.js

--- backend/models/FarmHealth.js
+++ backend/models/FarmHealth.js
@@ -1,10 +1,9 @@
-// DataBase OF far,er Health
 //Schema For farm health  using mongoose
 const mongoose = require("mongoose");
 
 const FarmHealthSchema = new mongoose.Schema({
     farmer: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    healthScore: { type: Number, default: 85 }, // %
+    healthScore: { type: Number, default: 85 }, 
     soilStatus: { type: String, default: "Soil moisture optimal, nutrients balanced." },
     soilMoisture: { type: String, default: "Optimal" },
     lastUpdated: { type: Date, default: Date.now },
